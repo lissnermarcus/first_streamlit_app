886e2eeb6b7ba19001af2742c85363a091e38811
lissnermarcus <80050048+lissnermarcus@users.noreply.github.com>
2022-10-17 10:56:06 +0200

    add fruit list as dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My parents new heathy diner")
 
@@ -10,3 +11,5 @@ streamlit.text("🐔 eggs for days")
 streamlit.text("🥑🍞 avocado toast")
 
 streamlit.header("🍌🥭 build your own fruit smoothie")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
