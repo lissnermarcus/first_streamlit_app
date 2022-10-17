5a87086eac8e27fdd25b893894920e668e751b51
lissnermarcus <80050048+lissnermarcus@users.noreply.github.com>
2022-10-17 11:01:45 +0200

    use fruit names as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text("🥑🍞 avocado toast")
 
 streamlit.header("🍌🥭 build your own fruit smoothie")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("fruit")
 #streamlit.dataframe(my_fruit_list)
 
 # lets put a pick list here so they can pick the fruit they want to include
